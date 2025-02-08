6319c4665e42850ca50333ffa73e1eb4ae926fb5
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2025-02-08 12:58:02 +0500

    ..

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BiSolidLogInCircle } from "react-icons/bi";
 
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="flex justify-evenly w-full h-screen  items-center">
+      <div>
+        <h1 className="font-bold text-3xl text-start"><span className="blue">Login</span> To
+          <br /> get Awesomes <span className="green">Loans</span></h1>
+      </div>
+      <div className="w-72 border border-black h-72">
+      <div className="flex justify-center align-middle">
+      <BiSolidLogInCircle   />
+      </div>
+        <input type="text" className="w-auto" />
+     <input type="text" />
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
